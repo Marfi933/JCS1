a645ced6dc90bfe2caaab2df912a7f9a13b72490
Petr Němec <50024077+Marfi933@users.noreply.github.com>
2023-05-02 12:34:36 +0200

    Add files via upload

---
+++ JCS02/JCS02/Program.cs
@@ -0,0 +1,137 @@
+﻿bool IsPrime (uint n)
+{
+    if (n <= 2) return true;
+    else
+    {
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0) return false;
+        }
+    }
+
+    return true;
+}
+
+void Sieve (uint n)
+{
+    string output = "";
+    for (uint i = 1; i <= n; i++)
+    {
+        if (IsPrime(i))
+        {
+            if (i != n && i!=1)
+            {
+                output += ", ";
+            }
+
+            output += i;
+        }
+    }
+    
+    Console.WriteLine(output);
+}
+
+void PrintSquare (int n)
+{
+    string output = "";
+    for (int row = 0; row < n; row++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (row == 0 || row == n - 1)
+            {
+                output += "*";
+            }
+            else
+            {
+                if (j == 0 || j == n - 1)
+                {
+                    output += '*';
+                }
+                else
+                {
+                    output += ' ';
+                }
+            }
+        }
+
+        output += '\n';
+    }
+    Console.WriteLine(output);
+}
+
+void PrintCross (int n)
+{
+    string output = "";
+    // mid is middle row index
+    int mid = ((n - 1) / 2) + 1;
+    for (int row = 1; row <= n; row++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (row == mid)
+            {
+                output += '*';
+            }
+
+            else
+            {
+                if (mid == j)
+                {
+                    output += '*';
+                }
+                else
+                {
+                    output += ' ';
+                }
+            }
+        }
+
+        output += '\n';
+    }
+
+    Console.WriteLine(output);
+}
+
+void Square (int n)
+{
+    if (n % 2 == 0)
+    {
+        PrintSquare(n);
+        return; // similiar like brake in loop
+    }
+    PrintCross(n);
+}
+
+uint NumberofOccurences (string str, char c)
+{
+    uint count = 0;
+    foreach (var ch in str)
+    {
+        if (ch == c) count++;
+    }
+
+    return count;
+}
+
+string StringTimes(string str, uint n)
+{
+    string output = "";
+    for (int i = 0; i < n; i++)
+    {
+        output += str;
+        if (i != n - 1)
+        {
+             output += ", ";
+        }
+    }
+
+    return output;
+}
+
+Sieve(60);
+Square(5);
+Square(4);
+Console.WriteLine(NumberofOccurences("Retezec", 'e'));
+Console.WriteLine(StringTimes("Retezec", 5));
+
---
+++ JCS1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿double Discriminant(double a, double b, double c)
+{
+    double x = b * b - 4 * a * c;
+    return x;
+}
+
+// print every number divided by 7
+void PrintNumbersDividedBy7(int i)
+{
+    int j = 0;
+    while (j < i)
+    {
+        if (j % 7 == 0 && j % 3 != 0)
+        {
+            Console.WriteLine(j);
+        }
+        j++;
+    }
+}
+
+// Fibonacci numbers
+uint Fib(uint n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return Fib(n - 1) + Fib(n - 2);
+    }
+}
+
+// sum of n natural numbers
+uint SumNaturalNumbers(uint n)
+{
+    uint sum = 0;
+    for (uint i = 0; i < n; i++)
+    {
+        sum += i;
+        
+        if (i == 25) Console.WriteLine($"The result for i=25 is: {sum}");
+    }
+    
+    return sum;
+}
+
+
+Console.WriteLine(Discriminant(6, 11, 1));
+Console.WriteLine("------------------------");
+PrintNumbersDividedBy7(359);
+Console.WriteLine("------------------------");
+Console.WriteLine(Fib(4));
+Console.WriteLine(SumNaturalNumbers(55));
